e1a8682838fa7df378540131a5ea196418ef3b6c
jakaria242 <jakaria.dev242@gmail.com>
2024-05-09 12:14:58 +0600

    about page design

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Div from '../../utilities/Div'
 import { TypeAnimation } from 'react-type-animation';
 import Image from '../../utilities/Image';
+import Team from './Team';
 
 const About = () => {
 
@@ -50,6 +51,46 @@ const About = () => {
             <Image className="w-[100%] h-[100%]" source="https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
           </Div>
         </Div>
+             
+            <Div className='bg-purple-100 container mx-auto flex flex-row py-16 font-primary gap-5 items-center justify-around'>
+
+                <Div className='flex flex-col items-center space-y-2'>
+                    <span className='text-7xl'>789</span>
+                    <span className='uppercase'>perfect posts</span>
+                </Div>
+
+                <Div className='flex flex-col items-center space-y-2'>
+                    <span className='text-7xl'>595</span>
+                    <span className='uppercase'>perfect posts</span>
+                </Div>
+
+                <Div className='flex flex-col items-center space-y-2'>
+                    <span className='text-7xl'>695</span>
+                    <span className='uppercase'>perfect posts</span>
+                </Div>
+
+                <Div className='flex flex-col items-center space-y-2'>
+                    <span className='text-7xl'>895</span>
+                    <span className='uppercase'>perfect posts</span>
+                </Div>
+            </Div>
+
+               {/* team part starts here */}
+            <Div className='grid grid-cols-12 container mx-auto'>
+                <Div className='col-span-12 '>
+                    <h2 className='font-primary text-4xl text-center font-bold pt-10'>Our Team</h2>
+                    <h6 className=' text-gray-400 font-primary text-center pb-10 pt-2 underline'>Meet With Our Nice People..</h6>
+                </Div>
+                <Div className='col-span-4 p-5'>
+                    <Team source="https://img.freepik.com/free-photo/brunette-businesswoman-posing_23-2148142754.jpg?t=st=1715234879~exp=1715238479~hmac=f0fabcf6565d94f5f4cddbe335119fc766dd1f113fd6f64ba051779947ddeb43&w=740" memberName="Diana" title="Artist" description="tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem." />
+                </Div>
+                <Div className='col-span-4 p-5'>
+                    <Team source="https://img.freepik.com/free-photo/portrait-corporate-woman-holding-clipboard-work-standing-formal-outfit-white-background_1258-88411.jpg?t=st=1715231552~exp=1715235152~hmac=daf3d4bd9acf7b28befbda26f5a977bdbe544d37cd79832418dcfa7eb6d8c0f0&w=740" memberName="Tisha" title="Artist" description="tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem." />
+                </Div>
+                <Div className='col-span-4 p-5'>
+                    <Team source="https://img.freepik.com/free-photo/proud-young-executive-ready-start_1139-303.jpg?t=st=1715231639~exp=1715235239~hmac=53d4c9fbac9eaed9b485ba5da23c48bc6eafc1e62f00d2991e1493246bfe7316&w=740" memberName="Jons" title="Artist" description="tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem." />
+                </Div>
+            </Div>
      </>
   )
 }
